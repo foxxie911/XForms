1b1a8c8cc1b377c680832b0c1c7e27b5802abb51
foxxie_911 <foxxie911.dev@proton.me>
2025-04-04 21:39:50 +0600

    database update & identity user customized

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace XForms.Data;
@@ -5,5 +6,8 @@ namespace XForms.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    [Required]
+    [StringLength(50, ErrorMessage = "Your display name must be lower than 50 characters")]
+    public required string DisplayName {get; set;}
 }
 
